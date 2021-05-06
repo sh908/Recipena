8c2b7fd306748601aa05919a861020ae765324e8
sh908 <guiyuansei@gmail.com>
2021-05-06 09:09:18 +0000

    devise, posts/new,index,show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cfb03023f566891efe308f841c06714b193c2865b20f98d376213a4929a4d6a34e4e9e7be6869a2ed75c74370852d6d890a21fcc986c2ab436a2f62587d28b7c'
